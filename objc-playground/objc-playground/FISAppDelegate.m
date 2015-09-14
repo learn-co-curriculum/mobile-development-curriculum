e51f210cfb10827ca3c76fc35f7d5b61b010750b
markedwardmurray <markedwardmurray@gmail.com>
2015-09-14 14:21:03 -0400

    save

--- objc-playground/objc-playground/FISAppDelegate.m
+++ objc-playground/objc-playground/FISAppDelegate.m
@@ -9,195 +9,33 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    NSInteger i = 0;
-    NSUInteger u = 0;
-    CGFloat f = 0.0;
-    BOOL isFalse = NO;
-    
-    NSInteger j = 1;
-    NSUInteger v = 1;
-    CGFloat g = 1.0;
-    BOOL isTrue = YES;
-    
-    NSLog(@"%li", i);
-    NSLog(@"%li", j);
-    NSLog(@"%lu", u);
-    NSLog(@"%lu", v);
-    NSLog(@"%f", f);
-    NSLog(@"%f", g);
-    NSLog(@"%d", isFalse);
-    NSLog(@"%d", isTrue);
-    
-    NSLog(@"i: %li", i);
-    NSLog(@"j: %li", j);
-    NSLog(@"u: %lu", u);
-    NSLog(@"v: %lu", v);
-    NSLog(@"f: %f", f);
-    NSLog(@"g: %f", g);
-    NSLog(@"isFalse: %d", isFalse);
-    NSLog(@"isTrue: %d", isTrue);
-    
-    i = 10;
-    j = -10;
-    u = 21;
-    v = 42;
-    f = 3.1415926536;
-    g = 1.6180398875;
-    isFalse = YES;
-    isTrue = NO;
-    
-    NSLog(@"i: %li", i);
-    NSLog(@"j: %li", j);
-    NSLog(@"u: %lu", u);
-    NSLog(@"v: %lu", v);
-    NSLog(@"f: %f", f);
-    NSLog(@"g: %f", g);
-    NSLog(@"isFalse: %d", isFalse);
-    NSLog(@"isTrue: %d", isTrue);
-
-    
-    NSInteger a = 1 + 1;
-    NSInteger b = 7 - 2;
-    NSInteger c = 3 * 4;
-    NSInteger d = 21 / 3;
-    NSInteger e = 13 % 7;
-    
-    NSLog(@"a: %li, b: %li, c: %li, d: %li, e: %li", a, b, c, d, e);
-    
-    NSInteger ab = a + b;
-    NSInteger cd = c * d;
-    
-    NSLog(@"ab: %li, cd: %li", ab, cd);
-    
-    NSInteger k = 0;
-    k++;
-    NSLog(@"k: %li", k);
-    k++;
-    NSLog(@"k: %li", k);
-    k++;
-    NSLog(@"k: %li", k);
-    
-    NSInteger l = 10;
-    l--;
-    NSLog(@"l: %li", l);
-    l--;
-    NSLog(@"l: %li", l);
-    l--;
-    NSLog(@"l: %li", l);
-
-    CGFloat m = 1.41421356237;
-    NSLog(@"m: %f", m);
-    m++;
-    NSLog(@"m: %f", m);
-    m++;
-    NSLog(@"m: %f", m);
-    m++;
-    NSLog(@"m: %f", m);
-    
-    NSInteger sum = 7;
-    NSLog(@"sum: %li", sum);
-    sum += 6;
-    NSLog(@"sum: %li", sum);
-    
-    NSInteger diff = 7;
-    NSLog(@"diff: %li", diff);
-    diff -= 10;
-    NSLog(@"diff: %li", diff);
-    
-    NSInteger product = 5;
-    NSLog(@"product: %li", product);
-    product *= 3;
-    NSLog(@"product: %li", product);
-    
-    NSInteger quotient = 27;
-    NSLog(@"quotient: %li", quotient);
-    quotient /= 5;
-    NSLog(@"quotient: %li", quotient);
-    
-    NSInteger remainder = 27;
-    NSLog(@"remainder: %li", remainder);
-    remainder %= 5;
-    NSLog(@"remainder: %li", remainder);
-    
-    
-    
-    BOOL isEqualTo            = 7 == 7;
-    BOOL isNotEqualTo         = 7 != 7;
-    BOOL lessThan             = 5 < 7;
-    BOOL lessThanOrEqualTo    = 7.0 <= 7;
-    BOOL greaterThan          = 8 > 7;
-    BOOL greaterThanOrEqualTo = 7.01 > 7;
-    
-    NSLog(@"isEqualTo: %d", isEqualTo);
-    NSLog(@"isNotEqualTo: %d", isNotEqualTo);
-    NSLog(@"lessThan: %d", lessThan);
-    NSLog(@"lessThanOrEqualTo: %d", lessThanOrEqualTo);
-    NSLog(@"greaterThan: %d", greaterThan);
-    NSLog(@"greaterThanOrEqualTo: %d", greaterThanOrEqualTo);
     
     
     
     
-    NSInteger manual = 6 + 7 * 8 - 2;
-    NSLog(@"manual: %li", manual);
+    for (NSUInteger i = 1; i <= 100; i++) {
+        NSString *result = @"";
+        if (i % 3 == 0) {
+            result = @"Fizz";
+        }
+        if (i % 5 == 0) {
+            result = [result stringByAppendingString:@"Buzz"];
+        }
+        if (result.length == 0) {
+            result = [NSString stringWithFormat:@"%lu", i];
+        }
+        NSLog(@"%@", result);
+    }
     
-    NSInteger override = (6 + 7) * 8 - 2;
-    NSLog(@"override: %li", override);
     
     
-    
-//    FISRadio *radio = [[FISRadio alloc] init];
-//    NSLog(@"AM: %.1f, FM: %.1f", radio.amFrequency, radio.fmFrequency);
-//    [radio increaseFrequencies];
-//    NSLog(@"AM: %.1f, FM: %.1f", radio.amFrequency, radio.fmFrequency);
-//    [radio decreaseFrequencies];
-//    NSLog(@"AM: %.1f, FM: %.1f", radio.amFrequency, radio.fmFrequency);
-//    [radio decreaseFrequencies];
-//    NSLog(@"AM: %.1f, FM: %.1f", radio.amFrequency, radio.fmFrequency);
-    
-//    FISWarship *ussMissouri = [[FISWarship alloc] initWithShipName:@"USS Missouri"
-//                                               currentSpeedInKnots:0
-//                                               maximumSpeedInKnots:33];
-//    
-//    NSLog(@"Ship's Name: %@", ussMissouri.shipName);
-//    NSLog(@"Current Speed: %lu", ussMissouri.currentSpeedInKnots);
-//    NSLog(@"Maximum Speed: %lu", ussMissouri.maximumSpeedInKnots);
-//    
-//    [ussMissouri aheadOneHalfSpeed];
-//    [ussMissouri fullStop];
-//    [ussMissouri fullSpeedAhead];
-//    
-////    NSSortDescriptor *sortByNameAsc = [NSSortDescriptor sortDescriptorWithKey:@"name"
-////                                                                    ascending:YES];
-//    
-////    NSSortDescriptor *sortByNameAsc = [[NSSortDescriptor alloc] initWithKey:@"name"
-////                                                                  ascending:YES];
-//    
-////    NSSortDescriptor *sortByNameAsc = [[NSSortDescriptor alloc] init];
-////    sortByNameAsc.key = @"name";
-////    sortByNameAsc.ascending = YES;
-//    
-//    NSArray *instructors = [[NSArray alloc] initWithObjects:(const id[5]){ @"Joe", @"Tim", @"Tom", @"Jim", @"Mark" } count:5];
-//    
-//    NSLog(@"%@", instructors);
-//
-//    NSMutableArray *instructors = [[NSMutableArray alloc] init];
-//    [instructors addObjectsFromArray:@[@"Joe", @"Tim", @"Tom", @"Jim", @"Mark"]];
-//
-//    NSLog(@"%@", instructors);
-//    
-//    FISWarship *ship = [[FISWarship alloc] init];
-//    FISWarship *boat = [[FISWarship alloc] init];
-//    
-//    NSLog(@"%@ - Max: %lu", ship.shipName, ship.maximumSpeedInKnots);
-//    NSLog(@"%@ - Max: %lu", boat.shipName, boat.maximumSpeedInKnots);
-//
-//    FISWarship *ussNewJersey = [[FISWarship alloc] initWithShipName:@"USS New Jersey"];
-//    FISWarship *ussWisconsin = [[FISWarship alloc] initWithShipName:@"USS Wisconsin"
-//                                                maximumSpeedInKnots:33];
-//    
-//    NSLog(@"%@ - Speed: %lu - Max: %lu", ussNewJersey.shipName, ussNewJersey.currentSpeedInKnots, ussNewJersey.maximumSpeedInKnots);
-//    NSLog(@"%@ - Speed: %lu - Max: %lu", ussWisconsin.shipName, ussWisconsin.currentSpeedInKnots, ussWisconsin.maximumSpeedInKnots);
+//    for (NSUInteger i = 1; i <= 100; i++) {
+//        NSString *result = @"";
+//        result = i % 3 == 0 ? @"Fizz" : result;
+//        result = i % 5 == 0 ? [result stringByAppendingString:@"Buzz"] : result;
+//        result = result.length > 0 ? result : [NSString stringWithFormat:@"%lu", i];
+//        NSLog(@"%@", result);
+//    }
     
     return YES;
 }
--- outline.md
+++ outline.md
@@ -31,35 +31,27 @@ SP = existing Spec lab
   01 Basic Objective-C --> Objective-C Fundamentals
     01 Xcode: Just Enough  
     02 CA: Your First NSLog
-    03 NSLog & String Literal & Comments ?
-        some students are confusing NSLog syntax for string interpolation methods, need to discuss () vs [] vs {}
-        nice-to-have: quiz
+    03 NSLog & String Literal
     04 CA: Interpolate
-        like: https://github.com/learn-co-curriculum/objc-basics
-    05 Variables  // discuss commenting too? /* */  (2 issues: discuss naming, add lab)
-    xc06 ca: code-along about warnings and errors
-    not now    ll CA: (naming practice) By Any Other Name
-    rr07 Primitives & Operands
-        should introduce integers and math before discussing nsnumber
-    ll08 CA: Simple Math
+    05 Variable Basics         (1 issues: add lab)
+    06 Xcode: Warnings and Errors
+    07 Primitives & Operators
+    08 CA: Simple Math
     09 Calling Methods
     10 NSString
-    11 CA: Parrot -- remove NSMutableString until later
-    gg12 fundamentals reference guide
+    11 CA: Parrot
+    12 RG: objc-fundamentals
     nice-to-have ll cap: objc-playground ?
 
   02 Basic Structures
     01 Xcode: An Overview (1 issue: reordered in curriculum since rewrite)
-    rr BOOL with quiz ?
-    02 Conditionals       (2 issues: introduce problem, add boolean quiz)
+    scrape the quiz from ruby --rr BOOL with quiz ?
+    02 Conditionals       (1 issues: add boolean quiz)
     03 CA: Speaking Grandma
     04 The for Loop       (2 issues: needs lab, introduce problem)
     ll CA: (looping) FizzBuzz ?
-    05 NSArray            (1 issues: needs lab)
-        remove NSMutableArray until later, teach arrayByAddingObject: for now
-    06 CA: WWDC Badges    (2 issues: make code-along, first time iterating over arrays)
-    07 NSNumber vs NSInteger
-    ll CA: (nsnumber practice)
+    05 NSArray & NSMutableArray (1 issue: needs lab)
+    06 WWDC Badges
     08 Creating Methods   (2 issues: rewrite example to use FISAppDelegate class, needs short lab)
     ll CA: (creating methods) palindrome detector ?
     00 NSMutableString
@@ -69,12 +61,12 @@ SP = existing Spec lab
     gg basic structure reference guide (+ fundamentals RG)
     11 Cap: Deli Counter  (2 issues: needs more explicit instructions, needs more previous labs)
 
-  03 Scope & Mutables
-    xc ? Xcode: Common error messages ?
-    rr NSMutableArray
-    ll fibo finder https://github.com/learn-co-curriculum/ios-fibo-finder
+  03 Scope
+    xc Xcode: Apple Docs-Foundation Methods
     01 Scope
     02 Practice With Scope
+    00 NSNumber
+    ll fibo finder https://github.com/learn-co-curriculum/ios-fibo-finder
     03 Xcode: Debugging
     04 Cartoon Collections (1 issues: debugging reminder)
     gg scope & mutables reference guide (+ basic structure RG + fundamentals RG)
@@ -87,12 +79,12 @@ SP = existing Spec lab
     04 Dictionary Ice Cream
     05 Nested Data Structures  (1 issue: needs use cases simple lab)
     ll ca: hashketball?
-    07 Apples & Holidays : without Apples section, just nested collection practice (1 issue: split into two labs)
-    08 NSPredicate             (1 issue: long/needs quiz or lab)
-    ll nspredicate practice: ? https://github.com/learn-co-curriculum/ios-collections-vol-2/
+    07 Apples & Holidays : without Apples section, just nested collection practice (2 issues: split into two labs, void methods)
     09 NSSortDescriptor        (1 issue: needs quiz or short lab)
     ll sorting practice
-    10 Collections Review      (1 issue: fix a test)
+    08 NSPredicate             (1 issue: long/needs quiz or lab)
+    ll nspredicate practice: ? https://github.com/learn-co-curriculum/ios-collections-vol-2/
+    10 Collections Review
     gg collections reference guide (+ scope & mutables RG + basic structure RG + fundamentals RG)
     11 Cap: Location Trivia Dictionaries
 
@@ -105,7 +97,7 @@ SP = existing Spec lab
     04 RR Property Basics
     05 SP Holiday Properties 
     06 RR Creating A Custom Class
-    07 SP iOShoppingCart        (1 issue: nssortdescriptor requires @selector)
+    07 SP iOShoppingCart
     xc ? xcode lesson ? pbxproj ? config files ?
     08 RR Property Visibility
     09 SP FISSentence
@@ -136,7 +128,7 @@ SP = existing Spec lab
     02 OO-Vehicle
     rr Overriding Inherited Methods
     03 OOP-Cards-Model
-    rr Casting
+    rr Casting & id
     04 PetCast
     // how to read the apple docs
     xc xcode: quick links to docs
